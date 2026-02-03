b8b106252d5019da961e46204a886d7975b9874c
thommiie <thomasjacobpadamadan@gmail.com>
2026-02-03 11:50:08 +0530

    commit for a reason

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -12,20 +12,21 @@ const StatisticLine = ({ text, value }) => (
   </tr>
 )
 
-const Statistics = ({ good, neutral, bad }) => {
-  const total = good + neutral + bad
+const Statistics = ({ good, neutral, bad,verygood }) => {
+  const total = good + neutral + bad + verygood
 
   if (total === 0) {
     return <p>No feedback given</p>
   }
 
-  const average = (good - bad) / total
-  const positive = (good / total) * 100
+  const average = ((good + verygood) - bad) / total
+  const positive = ((verygood+good) / total) * 100
 
   return (
     <table>
       <tbody>
         <StatisticLine text="good" value={good} />
+        <StatisticLine text="very good" value={verygood} />
         <StatisticLine text="neutral" value={neutral} />
         <StatisticLine text="bad" value={bad} />
         <StatisticLine text="all" value={total} />
@@ -40,6 +41,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [verygood, setVerygood] = useState(0)
 
   return (
     <div>
@@ -48,10 +50,11 @@ const App = () => {
       <Button text="good" onClick={() => setGood(good + 1)} />
       <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
       <Button text="bad" onClick={() => setBad(bad + 1)} />
+      <Button text="very good" onClick={() => setVerygood(verygood + 1)} />  
 
       <h1>statistics</h1>
 
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} verygood={verygood} />
     </div>
   )
 }
