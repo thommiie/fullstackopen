094303372d955fbba68c263d770ba0d9c654d2a3
thommiie <thomasjacobpadamadan@gmail.com>
2026-02-10 10:42:10 +0530

    yes commit this

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  const filteredCountries = countries.filter(country =>
+    country.name.common.toLowerCase().includes(search.toLowerCase())
+  )
+
+  const showCountry = (name) => {
+    setSearch(name)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      find countries
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <CountryList
+        countries={filteredCountries}
+        showCountry={showCountry}
+      />
+    </div>
   )
+}
+
+const CountryList = ({ countries, showCountry }) => {
+  if (countries.length > 10) {
+    return <p>Too many matches, specify another filter</p>
+  }
+
+  if (countries.length > 1) {
+    return (
+      <div>
+        {countries.map(country => (
+          <div key={country.cca3}>
+            {country.name.common}
+            <button onClick={() => showCountry(country.name.common)}>
+              show
+            </button>
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+  if (countries.length === 1) {
+    const country = countries[0]
+
+    return (
+      <div>
+        <h2>{country.name.common}</h2>
+        <p>capital {country.capital}</p>
+        <p>area {country.area}</p>
+
+        <h3>languages</h3>
+        <ul>
+          {Object.values(country.languages).map(lang => (
+            <li key={lang}>{lang}</li>
+          ))}
+        </ul>
+
+        <img src={country.flags.png} width="150" />
+      </div>
+    )
+  }
+
+  return null
 }
 
 export default App
