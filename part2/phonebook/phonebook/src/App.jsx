048e253bdcf1bb2522ad82c3b18665c06979b752
thommiie <thomasjacobpadamadan@gmail.com>
2026-02-10 10:20:46 +0530

    phonebook final

--- part2/phonebook/phonebook/src/App.jsx
+++ part2/phonebook/phonebook/src/App.jsx
@@ -1,7 +1,40 @@
+import { useState } from 'react'
+
 const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    setPersons(persons.concat(newName))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+
+      <ul>
+        {persons.map(person => (
+          <li key={person}>{person}</li>
+        ))}
+      </ul>
     </div>
   )
 }
