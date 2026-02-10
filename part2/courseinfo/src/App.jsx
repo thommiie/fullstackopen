c7b8ac224cf7e6567269c13be016c2ecdbaae719
thommiie <thomasjacobpadamadan@gmail.com>
2026-02-10 09:59:00 +0530

    is this why i committed?

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,9 +1,7 @@
 const Header = ({ course }) => <h1>{course}</h1>
 
 const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
+  <p>{part.name} {part.exercises}</p>
 )
 
 const Content = ({ parts }) => (
@@ -20,24 +18,45 @@ const Total = ({ parts }) => {
     0
   )
 
-  return <p>Number of exercises {total}</p>
+  return <p>Total of {total} exercises</p>
 }
 
-const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      { name: "Fundamentals of React", exercises: 10, id: 1 },
-      { name: "Using props to pass data", exercises: 7, id: 2 },
-      { name: "State of a component", exercises: 14, id: 3 }
-    ]
-  }
-
+const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        { name: "Fundamentals of React", exercises: 10, id: 1 },
+        { name: "Using props to pass data", exercises: 7, id: 2 },
+        { name: "State of a component", exercises: 14, id: 3 }
+      ]
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        { name: "Routing", exercises: 3, id: 1 },
+        { name: "Middlewares", exercises: 7, id: 2 }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      {courses.map(course => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
   )
 }
